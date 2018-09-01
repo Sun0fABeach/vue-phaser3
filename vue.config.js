fde974432e734b106e5b7b9000dfe7b11374bd25
Markus Appel <markospoparkos@gmail.com>
2018-09-01 14:26:43 +0200

    Tweak vue webpack config to accomodate Phaser

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack')
+
+module.exports = {
+  chainWebpack: config => {
+    /* disable insertion of assets as data urls b/c Phaser doesn't support it */
+    const rules = [
+      { name: 'images', dir: 'img' },
+      { name: 'media',  dir: 'media' }
+    ]
+    rules.forEach(rule => {
+      const ruleConf = config.module.rule(rule.name)
+
+      ruleConf.uses.clear()
+
+      ruleConf
+        .use('file-loader')
+          .loader('file-loader')
+          .options({
+            name: `${rule.dir}/[name].[hash:8].[ext]`
+          })
+    })
+  }
+}
