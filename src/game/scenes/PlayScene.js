9e77e3a2171efd38702bfff5320ef0fbb38cc410
Markus Appel <markospoparkos@gmail.com>
2018-09-02 10:46:12 +0200

    Game sample: replace sprite with image object

--- src/game/scenes/PlayScene.js
+++ src/game/scenes/PlayScene.js
@@ -9,7 +9,7 @@ export default class PlayScene extends Scene {
   create () {
     this.add.image(400, 300, 'sky')
 
-    const bomb = this.physics.add.sprite(400, 200, 'bomb')
+    const bomb = this.physics.add.image(400, 200, 'bomb')
     bomb.setCollideWorldBounds(true)
     bomb.body.onWorldBounds = true // enable worldbounds collision event
     bomb.setBounce(1)
