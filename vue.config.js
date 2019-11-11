a0d01daa4da6d015cef3a4f4927ce7e981584995
Markus Appel <markospoparkos@gmail.com>
2019-11-11 21:00:11 +0100

    Disable base64 inlining only for Phaser assets

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,29 @@
 module.exports = {
   chainWebpack: config => {
-    /* disable insertion of assets as data urls b/c Phaser doesn't support it */
+    /* disable insertion of assets as data urls if they are used by Phaser */
     const rules = [
-      { name: 'images', dir: 'img' },
-      { name: 'media',  dir: 'media' }
+      {
+        name: 'images',
+        dir: 'img',
+        test: /\.(png|jpe?g|gif|webp)(\?.*)?$/
+      },
+      {
+        name: 'media',
+        dir: 'media',
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/
+      }
     ]
+
     rules.forEach(rule => {
-      const ruleConf = config.module.rule(rule.name)
+      config.module.rule(rule.name)
+        .exclude
+          .add(/src\/game\/assets/)
 
-      ruleConf.uses.clear()
-
-      ruleConf
+      config.module.rule(`${rule.name}-phaser`)
+        .test(rule.test)
+        .include
+          .add(/src\/game\/assets/)
+          .end()
         .use('file-loader')
           .loader('file-loader')
           .options({
