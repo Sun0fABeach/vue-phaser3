6e63f663cb61e0b2365fdb354108e9d9176cf2ef
Markus Appel <markospoparkos@gmail.com>
2024-05-28 14:54:26 +0200

    fix vue-cli feature flag warning

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,18 @@
+const webpack = require('webpack');
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        // Vue CLI is in maintenance mode and probably won't fix this
+        // https://stackoverflow.com/questions/77752897/feature-flag-vue-prod-hydration-mismatch-details-is-not-explicitly-defined
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+      })
+    ],
+  },
 
   chainWebpack: config => {
     /* disable insertion of assets as data urls b/c Phaser doesn't support it */
