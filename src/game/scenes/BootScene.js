9cb4916484abbbe1706fa743131164f292056a57
Markus Appel <markospoparkos@gmail.com>
2025-02-26 18:22:50 +0100

    remove sample audio in ogg format

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -1,8 +1,7 @@
 import { Scene } from 'phaser'
 import sky from '@/game/assets/sky.png'
 import bomb from '@/game/assets/bomb.png'
-import thudMp3 from '@/game/assets/thud.mp3'
-import thudOgg from '@/game/assets/thud.ogg'
+import thud from '@/game/assets/thud.mp3'
 
 export default class BootScene extends Scene {
   constructor () {
@@ -12,7 +11,7 @@ export default class BootScene extends Scene {
   preload () {
     this.load.image('sky', sky)
     this.load.image('bomb', bomb)
-    this.load.audio('thud', [thudMp3, thudOgg])
+    this.load.audio('thud', thud)
   }
 
   create () {
