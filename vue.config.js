ecc0b500aef9424cd96f5af77b25627ac823b456
Markus Appel <markospoparkos@gmail.com>
2025-02-26 18:15:50 +0100

    allow data url assets

--- vue.config.js
+++ vue.config.js
@@ -14,16 +14,6 @@ module.exports = defineConfig({
     ],
   },
 
-  chainWebpack: config => {
-    /* disable insertion of assets as data urls b/c Phaser doesn't support it */
-    const rules = ['images', 'media']
-
-    rules.forEach(rule => {
-      const ruleConf = config.module.rule(rule)
-      ruleConf.type('asset/resource')
-    })
-  },
-
   devServer: {
     hot: false
   }
